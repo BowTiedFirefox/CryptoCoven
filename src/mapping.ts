b3099f961e21d800f121b9d462e789b613754b8c
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2022-01-25 23:12:11 -0500

    update schema

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,31 +5,18 @@ import {
   Transfer as TransferEvent
 } from "../generated/CryptoCoven/CryptoCoven"
 import {
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Transfer
+  Account,
+  Token
 } from "../generated/schema"
 
 
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
-
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-  entity.save()
+  // let entity = new Transfer(
+  //   event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  // )
+  // entity.from = event.params.from
+  // entity.to = event.params.to
+  // entity.tokenId = event.params.tokenId
+  // entity.save()
 }
