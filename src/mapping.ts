0e65a568c6a4053da881cd13a4216f13ec8f3968
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2022-02-01 00:51:57 -0500

    finalized subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,39 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
   Transfer as TransferEvent
 } from "../generated/CryptoCoven/CryptoCoven"
 import {
   Account,
   Token
 } from "../generated/schema"
+import {BigInt} from "@graphprotocol/graph-ts";
 
 
 
 export function handleTransfer(event: TransferEvent): void {
-  // let entity = new Transfer(
-  //   event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  // )
-  // entity.from = event.params.from
-  // entity.to = event.params.to
-  // entity.tokenId = event.params.tokenId
-  // entity.save()
+  let token = Token.load(event.params.tokenId.toString());
+  if (!token) {
+    token = new Token(event.params.tokenId.toString());
+  }
+  token.owner = event.params.to.toHexString();
+  token.covenIndex = event.params.tokenId;
+  token.save();
+  let accountTo = Account.load(event.params.to.toHexString());
+  if (!accountTo) {
+    accountTo = new Account(event.params.to.toHexString());
+    accountTo.covenNumber = BigInt.fromI32(0);
+  }
+  accountTo.covenNumber = accountTo.covenNumber.plus(
+      BigInt.fromI32(1)
+  );
+  accountTo.save();
+
+  let accountFrom = Account.load(event.params.from.toHexString());
+  if (!accountFrom) {
+    accountFrom = new Account(event.params.from.toHexString());
+    accountFrom.covenNumber = BigInt.fromI32(0);
+  }
+  accountFrom.covenNumber = accountFrom.covenNumber.minus(
+      BigInt.fromI32(1)
+  );
+  accountFrom.save()
 }
